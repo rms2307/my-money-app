959a28eed0e7683274fccd1aff2304bde6e46133
Robson de Moraes Silva <rms2307@users.noreply.github.com>
2020-12-12 18:35:21 -0300

    frontend

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'> 
+        <strong> 
+            Robson &copy; 2020
+            <a href='https://github.com/rms2307' target='_blank'> My Git</a>.
+        </strong>
+    </footer>
+)
