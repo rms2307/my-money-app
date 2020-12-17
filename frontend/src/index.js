63b85beb7c12ab52ac2d75ad3abcb69cc87d72f9
Robson de Moraes Silva <rms2307@outlook.com>
2020-12-17 20:25:17 -0300

    atualizando bibliotecas

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,20 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { applyMiddleware, createStore } from 'redux'
 import { Provider } from 'react-redux'
+import reportWebVitals from './reportWebVitals'
 
 import promise from 'redux-promise'
 import multi from 'redux-multi'
 import thunk from 'redux-thunk'
 
 import reducers from './main/reducers'
-import Routes from './main/routes'
+import AuthOrApp from './main/authOrApp'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
-    && window.__REDUX_DEVTOOLS_EXTENSION__()
+  && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devTools)
 ReactDOM.render(
-    <Provider store={store}>
-        <Routes />
-    </Provider>
-    , document.getElementById('app'))
+  <Provider store={store}>
+    <AuthOrApp />
+  </Provider>
+  , document.getElementById('app'))
+
+reportWebVitals()
